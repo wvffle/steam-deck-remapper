2492794ebc3ab4f3ce742b53979ace4e6a884504
Kasper Seweryn <github@wvffle.net>
2024-04-29 17:39:15 +0200

    feat: add systemd service

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -1,26 +1,28 @@
 self: { config, pkgs, lib, ... }: with lib; let
-  cfg = config.programs.steam-deck-remapper;
+  cfg = config.services.steam-deck-remapper;
   format = pkgs.formats.toml {};
 in {
-  options.programs.steam-deck-remapper = {
-    enable = mkEnableOption "Steam Deck input remapper";
+  options = {
+    services.steam-deck-remapper = {
+      enable = mkEnableOption "Steam Deck input remapper";
 
-    package = mkOption {
-      type = types.nullOr types.package;
-      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
-      defaultText = literalExpression "inputs.steam-deck-remapper.packages.${pkgs.stdenv.hostPlatform.system}.default";
-      description = ''
-        The steam-deck-remapper package to use.
-                
-        By default, this option is set to the steam-deck-remapper package for your host system.
-      '';
-    };
+      package = mkOption {
+        type = types.nullOr types.package;
+        default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+        defaultText = literalExpression "inputs.steam-deck-remapper.packages.${pkgs.stdenv.hostPlatform.system}.default";
+        description = ''
+          The steam-deck-remapper package to use.
+                  
+          By default, this option is set to the steam-deck-remapper package for your host system.
+        '';
+      };
 
-    settings = mkOption {
-      inherit (format) type;
-      default = {};
-      example = builtins.fromTOML (builtins.readFile (self + /config.example.toml));
-      description = "Settings for Steam Deck input remapper";
+      settings = mkOption {
+        inherit (format) type;
+        default = {};
+        example = builtins.fromTOML (builtins.readFile (self + /config.example.toml));
+        description = "Settings for Steam Deck input remapper";
+      };
     };
   };
 
@@ -28,6 +30,20 @@ in {
     home.packages = [
       cfg.package
     ];
+
+    systemd.user.services."steam-deck-remapper" = lib.mkIf cfg.enable {
+      Unit = {
+        Description = "Systemd service for Steam Deck input remapper";
+        Requires = ["graphical-session.target"];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${cfg.package}/bin/steam-deck-remapper";
+      };
+
+      Install.WantedBy = ["graphical-session.target"];
+    };
     
     xdg.configFile."steam-deck-remapper/config.toml".source = format.generate "config.toml" cfg.settings;
   };
